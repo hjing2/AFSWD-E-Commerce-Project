d559d72690cb4d5c99c912f7a5a3ea30288ce9fa
hjing2 <hjing2@academic.rrc.ca>
2024-08-04 15:36:54 -0500

    0804 added filter to new/sale/updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,28 @@
 class ProductsController < ApplicationController
   def index
     @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true).page(params[:page]).per(10)
+    @products = @q.result(distinct: true)
 
-    if params[:on_sale].present?
-      @products = @products.where(is_on_sale: true).per(10)
+    Rails.logger.debug "Initial products count: #{@products.count}"
+
+    if params[:new_products] == "1"
+      @products = @products.where("created_at >= ?", 3.days.ago)
+      Rails.logger.debug "After new products filter: #{@products.count}"
     end
 
-    if params[:is_new].present?
-      @products = @products.where(is_new: true).per(10)
+    if params[:recently_updated] == "1"
+      @products = @products.where("updated_at >= ? AND created_at < ?", 3.days.ago, 3.days.ago)
+      Rails.logger.debug "After recently updated filter: #{@products.count}"
     end
 
-    if params[:is_recently_updated].present?
-      @products = @products.where(is_recently_updated: true).per(10)
+    if params[:on_sale] == "1"
+      @products = @products.where(is_on_sale: true)
+      Rails.logger.debug "After on sale filter: #{@products.count}"
     end
+
+    Rails.logger.debug "Final products count: #{@products.count}"
+
+    @products = @products.page(params[:page]).per(10)
   end
 
   def show
